ad35828d6912117648351ae7caa6f60c0227af4f
gitTaynara4x <git.taynara.4x4@gmail.com>
2024-10-04 15:05:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify
 import requests
 import os
 from dotenv import load_dotenv
-import time
 
 app = Flask(__name__)
 
@@ -17,47 +16,43 @@ BASE_URL_API_BITRIX = os.getenv('BASE_URL_API_BITRIX')
 BITRIX_WEBHOOK_URL = f"{BASE_URL_API_BITRIX}/{PROFILE}/{CODIGO_BITRIX}/bizproc.workflow.start"
 
 WORKFLOW_IDS = {
-    "workflow1": "1196", #primeiro boleto(1.1)
-    "workflow2": "1200", #primeiro boleto(1.2)
-    "workflow3": "1204", #segundo boleto(1.1)
-    "workflow4": "1206", #segundo boleto(1.2)
-    "workflow5": "1208", #terceiro boleto(1.1)
-    "workflow6": "1210", #terceiro boleto(1.2)
-    "workflow7": "1212", #quarto boleto(1.1)
-    "workflow8": "1214", #quarto boleto(1.2)
-    "workflow9": "1216", #quinto boleto(1.1)
-    "workflow10": "1218", #quinto boleto(1.2)
-    "workflow11": "1294" #CONSULTORES
+    "workflow1": "1196",
+    "workflow2": "1200",
+    "workflow3": "1204",
+    "workflow4": "1206",
+    "workflow5": "1208",
+    "workflow6": "1210",
+    "workflow7": "1212",
+    "workflow8": "1214",
+    "workflow9": "1216",
+    "workflow10": "1218",
+    "workflow11": "1294"
 }
 
 @app.route('/webhook/<workflow_name>', methods=['POST'])
 def start_workflow(workflow_name):
-    print("Webhook acionado!")  # Log to check if the endpoint is called
-    deal_ids = request.json.get('deal_ids')  # Expecting JSON body with a "deal_ids" array
+    print("Webhook acionado!") 
+    deal_ids = request.json.get('deal_ids')  
 
     if not deal_ids:
         return jsonify({"error": "deal_ids não fornecido"}), 400
 
-    # Get the workflow ID from the dictionary
     workflow_id = WORKFLOW_IDS.get(workflow_name)
     if not workflow_id:
         return jsonify({"error": "Workflow não encontrado"}), 404
 
-    responses = []  # Store responses for each deal_id
+    responses = []  
     for deal_id in deal_ids:
         array = ["crm", "CCrmDocumentDeal", f"DEAL_{deal_id}"]
         data = {
             "TEMPLATE_ID": workflow_id,
             "DOCUMENT_ID": array
         }
-
-        # Log the data being sent to Bitrix for debugging
         print(f"Sending data to Bitrix for deal_id {deal_id}: {data}")
-
         try:
             response = requests.post(BITRIX_WEBHOOK_URL, json=data)
-            response.raise_for_status()  # Raise an error for bad responses
-            responses.append(response.json())  # Append each response
+            response.raise_for_status()
+            responses.append(response.json()) 
         except requests.exceptions.RequestException as e:
             print(f"Error calling Bitrix API for deal_id {deal_id}: {e}")
             responses.append({"deal_id": deal_id, "error": str(e)})
@@ -66,33 +61,29 @@ def start_workflow(workflow_name):
 
 @app.route('/webhook2/', methods=['POST'])
 def start_workflow2():
-    print("Webhook acionado!")  # Log to check if the endpoint is called
-    deal_ids = request.args.get('deal_ids')  # Expecting JSON body with a "deal_ids" array
+    print("Webhook acionado!")
+    deal_ids = request.json.get('deal_ids')  
 
     if not deal_ids:
         return jsonify({"error": "deal_ids não fornecido"}), 400
 
-    # Get the workflow ID from the dictionary
-    workflow_name = request.args.get('workflow_name')
+    workflow_name = request.json.get('workflow_name')
     workflow_id = WORKFLOW_IDS.get(workflow_name)
     if not workflow_id:
         return jsonify({"error": "Workflow não encontrado"}), 404
 
-    responses = []  # Store responses for each deal_id
+    responses = []  
     for deal_id in deal_ids:
         array = ["crm", "CCrmDocumentDeal", f"DEAL_{deal_id}"]
         data = {
             "TEMPLATE_ID": workflow_id,
             "DOCUMENT_ID": array
         }
-
-        # Log the data being sent to Bitrix for debugging
         print(f"Sending data to Bitrix for deal_id {deal_id}: {data}")
-
         try:
             response = requests.post(BITRIX_WEBHOOK_URL, json=data)
-            response.raise_for_status()  # Raise an error for bad responses
-            responses.append(response.json())  # Append each response
+            response.raise_for_status()
+            responses.append(response.json())
         except requests.exceptions.RequestException as e:
             print(f"Error calling Bitrix API for deal_id {deal_id}: {e}")
             responses.append({"deal_id": deal_id, "error": str(e)})
